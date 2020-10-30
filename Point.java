b748a35f933c3a2b189e3e0dfb8a2e253994d7ba
jasonjiangstuy <jasonjiangny@gmail.com>
2020-10-29 21:36:14 -0400

    2.3 done

--- Point.java
+++ Point.java
@@ -1,3 +1,35 @@
-public class Point {
+public class Point{
+    private double x,y;
+
+    //construct a point given coordinates
+    public Point(double X, double Y){
+        x=X;
+        y=Y;
+    }
+
+    //construct a point given a point  (redundant , just provides flexibility)
+    public Point(Point p){
+        x= p.x;  //private access in the same file is allowed!
+        y= p.y;
+      }
+    
+    public double getX(){
+        return x;
+    }
+    
+    public double getY(){
+        return y;
+    }  
+
+    public double distanceTo(Point other){
+        return (double)
+        (Math.round(Math.sqrt(
+            Math.pow(x - other.x, 2) 
+            +
+            Math.pow(y - other.y, 2)    
+                    ) 
+            * 10000.0)
+            /10000.0);
+    }
     
 }
